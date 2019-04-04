2c2429fd3f21ebdd1335c601ef959772730a127e
Jose Manuel Ruiz Luque <Usuario@MSI>
2019-04-04 22:53:01 +0200

    4ª Version (FINAL ENTREGA 1)

--- src/entidades/Socio.java
+++ src/entidades/Socio.java
@@ -20,7 +20,7 @@ import javax.persistence.OneToMany;
  * @author Usuario
  */
 @Entity
-@DiscriminatorValue("U")
+@DiscriminatorValue("S")
 public class Socio extends Usuario implements Serializable {
 
     private static final long serialVersionUID = 1L;
